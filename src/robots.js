5bce4f9319461329e88684f85e05d57b8c142d22
Aresgod112 <31764360+Aresgod112@users.noreply.github.com>
2020-11-26 16:32:56 +0200

    Robofriends app

---
+++ src/robots.js
@@ -0,0 +1,55 @@
+export const robots = [{
+        id: 1,
+        name: '1234',
+        username: 'Doe',
+        email: 'Jane.Doe@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Jannnne',
+        username: 'Doeeee',
+        email: 'Jane.Doedwdwd@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Alalal',
+        username: 'Doeeee',
+        email: 'Jane.De@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Janddde',
+        username: 'Dceceoe',
+        email: 'Jane.Dcseceoe@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Nana',
+        username: 'Nananame',
+        email: 'Nana.W@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Xander',
+        username: 'XnderX',
+        email: 'Xanswe@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Jamie',
+        username: 'Jamie1000',
+        email: 'jamie@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Raoul',
+        username: 'RaoulJ',
+        email: 'RaoulJ@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Daniel',
+        username: 'DanielF',
+        email: 'DanielF@gmail.com'
+    },
+];
